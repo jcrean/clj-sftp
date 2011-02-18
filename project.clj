e4ea6e2d494c056b33e013696a1a309732f417a6
Joshua Crean <jcrean@Relay-Laptop-4.local>
2011-02-18 08:55:40 -0500

    adding dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject clj-sftp "1.0.0-SNAPSHOT"
   :description "Easy SFTP interface for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.jcraft/jsch "0.1.44"]
+                 [commons-vfs "1.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
